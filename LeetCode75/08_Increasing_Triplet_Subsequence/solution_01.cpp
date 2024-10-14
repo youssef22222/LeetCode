e6d094e07e308dfda637e5471f903bbb5f0abd4e
Youssef Elsayed <57481583+youssef22222@users.noreply.github.com>
2024-10-14 23:11:42 +0300

    Update solution_01.cpp

--- LeetCode75/08_Increasing_Triplet_Subsequence/solution_01.cpp
+++ LeetCode75/08_Increasing_Triplet_Subsequence/solution_01.cpp
@@ -1,1 +1,17 @@
-// Write your solution here
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int numI = INT_MAX;
+        int numJ = INT_MAX;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] <= numI) {
+                numI = nums[i];
+            } else if (nums[i] <= numJ) {
+                numJ = nums[i];
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+};
