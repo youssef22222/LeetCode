94cbcecdefe36dbfb12f53fdcf35ab7aedc82631
Youssef Elsayed <57481583+youssef22222@users.noreply.github.com>
2024-10-14 23:12:37 +0300

    Update solution_01.cpp

--- LeetCode75/36_Path_Sum_III/solution_01.cpp
+++ LeetCode75/36_Path_Sum_III/solution_01.cpp
@@ -1,1 +1,34 @@
-// Write your solution here
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int dfs(TreeNode* node, int targetSum, long long curSum) {
+        if (node == nullptr) {
+            return 0;
+        }
+        return (dfs(node->left, targetSum, curSum + node->val) +
+                dfs(node->right, targetSum, curSum + node->val) +
+                (curSum + node->val == targetSum));
+    }
+    int count(TreeNode* node, int targetSum) {
+        if (node == nullptr) {
+            return 0;
+        }
+        return dfs(node, targetSum, 0) + count(node->left, targetSum) +
+               count(node->right, targetSum);
+    }
+    int pathSum(TreeNode* root, int targetSum) {
+        // suppose each node is a root node start sum from each node
+        return count(root, targetSum);
+    }
+};
