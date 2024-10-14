0b017c9e847e168f58c45e269863aeab085ce01b
Youssef Elsayed <57481583+youssef22222@users.noreply.github.com>
2024-10-14 23:15:20 +0300

    Update solution_01.cpp

--- LeetCode75/51_Maximum_Subsequence_Score/solution_01.cpp
+++ LeetCode75/51_Maximum_Subsequence_Score/solution_01.cpp
@@ -1,1 +1,26 @@
-// Write your solution here
+class Solution {
+public:
+    long long maxScore(vector<int>& nums1, vector<int>& nums2, int k) {
+        int len = nums1.size();
+        vector<pair<int, int>> arr(len);
+        for (int i = 0; i < len; i++) {
+            arr[i] = {nums2[i], nums1[i]};
+        }
+        priority_queue<int, vector<int>, greater<int>> pq;
+        sort(arr.rbegin(), arr.rend());
+        long long mxVal = 0;
+        long long sum = 0;
+        for (int i = 0; i < len; i++) {
+            sum += arr[i].second;
+            if (pq.size() == k) {
+                sum -= pq.top();
+                pq.pop();
+            }
+            pq.push(arr[i].second);
+            if (pq.size() == k) {
+                mxVal = max(mxVal, sum * arr[i].first);
+            }
+        }
+        return mxVal;
+    }
+};
